ae550bf91b4e333badca1a0f5d8b8157b492c908
RodrigoGarciaNunez <118324022+RodrigoGarciaNunez@users.noreply.github.com>
2024-05-20 21:29:02 -0600

    Add files via upload

---
+++ cognitive_architecture/scripts/Controlador_individuo.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import rclpy
+from rclpy import executors
+from rclpy.node import Node
+from std_msgs.msg import String
+from gazebo_msgs.srv import SpawnEntity
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Range
+import sys
+import time
+#import keyboard
+import random as rd
+from threading import Thread
+from cognitive_architecture.lobulo_temporal import temporal_lobe as tl
+from cognitive_architecture.corteza_motora_primaria import Nodo as ci 
+
+#from arlo_interfaces.msg import PesosStruct
+
+def main(args=None):
+    rclpy.init(args=args) #inicia comunicaciones de ros2
+
+    if args is None:
+        import sys
+        args = sys.argv[1:] 
+    
+    limite=2
+    if args[0]=="1":
+        limite=3
+
+    nodos_control=[]
+    nodos_tl=[]
+    for i in range(1,limite):
+        nodo = ci(i,args[0])
+        lobulo_t = tl(i,args[0])
+        nodos_control.append(nodo)
+        nodos_tl.append(lobulo_t)
+
+    #print("perrrro voy bien")
+    executor = executors.MultiThreadedExecutor()
+    #print("perrrro voy bien 1")
+    for i in range(len(nodos_control)):
+        executor.add_node(nodos_control[i])
+        executor.add_node(nodos_tl[i])
+
+    #print("perrrro voy bien 2")
+    hilo = Thread(target=executor.spin)
+    #print("perrrro voy bien 3")
+    hilo.start()
+    #print("perrrro voy bien 4")
+    hilo.join()
+    #print("perrrro voy bien 5")
+
+    rclpy.shutdown() #termina comunicaciones de ros2
+
+
+if __name__ == '__main__':
+    main()
---
+++ cognitive_architecture/src/controlador_cpp.cpp
@@ -0,0 +1,157 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/range.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <string>
+#include <memory>
+#include <thread>
+#include <vector>
+#include "cognitive_architecture/corteza_premotora.hpp"
+//#include "cognitive_architecture/Red_Neuronal/NeuroControllerDriver.hpp"
+#include "cognitive_architecture/corteza_motora_secundaria.hpp"
+#include "fmt/core.h"
+#include "arlo_interfaces/msg/pesos_struct.hpp"
+#include "cognitive_architecture/control_simulacion/srvEvaluateDriver.hpp"
+#include <csignal>
+#include <cstdlib>
+#include "std_srvs/srv/empty.hpp"
+
+
+void remSignal(int signal){
+
+
+    rclcpp::executors::SingleThreadedExecutor executor;
+
+    bool bandera=true;    
+
+    std::vector<std::shared_ptr<rclcpp::Node>> nodes_cp;
+    
+
+    // std::cerr<< "soy la senal de rem" << std::endl;
+    //auto client = rclcpp::Node::make_shared("activacion_client");
+    //auto service;
+    // 
+    for(int i=1; i<2; i++){
+        RobotNaviFun* p = new RobotNaviFun();
+        ParamsGA params;
+        nodes_cp.push_back(std::make_shared<cp>(i, p, params, &bandera));
+        //executor.add_node(nodes_cp[i-1]);
+    }
+    //std::shared_ptr<rclcpp::Node> server = std::make_shared<srvEvaluateDriver>();
+    //executor.add_node(server);
+    std::vector<std::thread> threadsc;
+
+    //executor.spin();
+
+    for(int i=0; i< static_cast<int>(nodes_cp.size()); i++){
+
+        //service = client->create_client<std_srvs::srv::Empty>("robot%d0_activacion",i+1);
+        
+        threadsc.push_back(std::thread([&nodes_cp, i, &executor, &bandera]() {  //hay que checar que para más robots la bandera sea independiente
+            while(bandera==true){
+                //nodes_cp[i].ejecutar();
+                executor.spin_node_once(nodes_cp[i]);
+            }
+            //rclcpp::spin(nodes_cp[i]);
+            
+        }));
+
+    }   
+
+    //executor
+
+    std::shared_ptr<rclcpp::Node> server = std::make_shared<srvEvaluateDriver>();
+    rclcpp::spin(server);
+
+    for (auto& thread : threadsc) {
+        thread.join();
+    }
+
+}
+
+
+int main(int argc, char **argv){
+    
+
+    if (argc < 2){
+        std::cerr << "falta indicar qué tipo de nodos serán: 0=individuos 1=dummys" << std::endl;
+
+        return 0;
+    }
+    // Si en la línea de comandos se escribió un puerto, entonces asignarlo.
+   
+    //string URI = fmt::format("http://localhost:1135{}", 111);
+    //setenv("ROS_MASTER_URI", URI.c_str(), 1);
+
+    // Iniciar ROS en el puerto dado arriba.
+
+    rclcpp::init(argc, argv);
+
+    std::signal(SIGUSR1, remSignal);
+    int tipo = std::atoi(argv[1]);
+    //RobotNaviFun* p = new RobotNaviFun();
+    //ParamsGA params;
+    //ros::init(argc, argv, "neurocontroller_node");
+    //int arg = std::stoi(argv[1]);
+    //cerr << arg << endl;
+    int limite=2;
+    if(tipo==1){
+        limite=3;
+    }
+    std::vector<std::shared_ptr<rclcpp::Node>> nodes_cms;
+    //std::vector<std::shared_ptr<rclcpp::Node>> nodes_cp;
+    // Crear nodos y agregarlos a la lista
+    for(int i=1; i< limite; i++){
+        nodes_cms.push_back(std::make_shared<cmSec>(i,*argv[1]));
+    }
+    //nodes_cms.push_back(std::make_shared<cmSec>(1));
+    //nodes_cms.push_back(std::make_shared<cmSec>(2));
+    
+    //if(arg == 1){ //si se indica que el robot está soñando
+        // Crear nodos y agregarlos a la lista
+        //nodes_cp.push_back(std::make_shared<cp>(1, p, params));
+        //nodes_cp.push_back(std::make_shared<cp>(2, p, params));
+    //}
+    
+
+    
+    //auto node=std::make_shared<cmSec>();
+    
+    std::vector<std::thread> threads;
+    
+    for (int i=0; i< static_cast<int>(nodes_cms.size()); i++){
+        threads.push_back(std::thread([&nodes_cms, i]() {
+            rclcpp::spin(nodes_cms[i]);
+        }));
+        //threads.push_back(std::thread([&nodes_cp, i]() {
+        //    rclcpp::spin(nodes_cp[i]);
+        //}));
+
+    }
+
+    
+    if(tipo==0){
+        
+        char senal;
+        // // Leer la entrada del usuario desde la consola
+        std::cin >> senal;
+
+        if (senal=='1'){
+        std::raise(SIGUSR1);
+        }   
+
+    }
+    
+
+    //std::shared_ptr<rclcpp::Node> server = std::make_shared<srvEvaluateDriver>();
+
+    
+    //rclcpp::spin(server);
+
+    // Esperar a que todos los hilos terminen
+    for (auto& thread : threads) {
+        thread.join();
+    }
+    rclcpp::shutdown();
+
+    return 0;
+}
